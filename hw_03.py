c5ed1f80c05283153cc9c517aba6be5502a622f3
Mukta-Glitch <112215589+Mukta-glitch@users.noreply.github.com>
2023-03-11 05:51:12 +0000

    Final Commit Homework 3

--- hw_03.py
+++ hw_03.py
@@ -1,14 +1,25 @@
-import sys
+'''references=
+1.https://www.crowdstrike.com/blog/deep-dive-into-custom-spark-transformers-for-machine-learning-pipelines/
+2.https://spark.apache.org/docs/1.6.0/ml-guide.html
+3.https://teaching.mrsharky.com/sdsu_fall_2020_lecture06.html#/7/11/5
+4.https://www.youtube.com/watch?v=jEyahxFp3ak
+5.https://towardsdatascience.com/machine-learning-with-pyspark-and-mllib-solving-a-binary-classification-problem-96396065d2aa
+6.https://api-docs.databricks.com/python/pyspark/latest/api/pyspark.ml.evaluation.BinaryClassificationEvaluator.html
+'''
 
+
+import sys
 from pyspark import StorageLevel, keyword_only
 from pyspark.ml import Pipeline, Transformer
 from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.feature import CountVectorizer
-from pyspark.ml.feature import FeatureHasher
 from pyspark.ml.param.shared import HasInputCols, HasOutputCol
 from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, concat, lit, split, when
+from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
+from pyspark.sql.functions import round
+from pyspark.ml.linalg import Vectors
 
 import warnings
 warnings.simplefilter(action='ignore')
@@ -17,10 +28,10 @@ appName = "Pyspark Homework 3"
 master = "local"
 # Create Spark session
 spark = SparkSession.builder \
-    .appName(appName) \
-    .master(master) \
-    .getOrCreate()
-
+	.appName(appName) \
+	.master(master) \
+	.getOrCreate()
+# Define table from database and set JDBC connection 
 sql = "select * from baseball.batter_counts"
 sql1="select * from baseball.game"
 database = "baseball"
@@ -31,67 +42,133 @@ port = "3306"
 jdbc_url = f"jdbc:mysql://{server}:{port}/{database}?permitMysqlScheme"
 jdbc_driver = "org.mariadb.jdbc.Driver"
 
-# Create a data frame by reading data from Mariadb via JDBC
+# Create a data frame of batting_df by reading data from Mariadb via JDBC
 batting_df = spark.read.format("jdbc") \
-    .option("url", jdbc_url) \
-    .option("query", sql) \
-    .option("user", user) \
-    .option("password", password) \
-    .option("driver", jdbc_driver) \
-    .load()
+	.option("url", jdbc_url) \
+	.option("query", sql) \
+	.option("user", user) \
+	.option("password", password) \
+	.option("driver", jdbc_driver) \
+	.load()
 
 batting_df.select("batter", "game_id", "atBat", "Hit").show()
 batting_df.createOrReplaceTempView("batter_view")
-batting_df.persist(StorageLevel.MEMORY_ONLY)
+batting_df.persist(StorageLevel.MEMORY_AND_DISK)
 
-rolling_df = spark.read.format("jdbc") \
-    .option("url", jdbc_url) \
-    .option("query", sql1) \
-    .option("user", user) \
-    .option("password", password) \
-    .option("driver", jdbc_driver) \
-    .load()
-    
-rolling_df.select("game_id", "local_date").show()
-rolling_df.createOrReplaceTempView("game")
-rolling_df.persist(StorageLevel.MEMORY_ONLY)
+#Create a data frame of game by reading data from Mariadb via JDBC
+
+game_df = spark.read.format("jdbc") \
+	.option("url", jdbc_url) \
+	.option("query", sql1) \
+	.option("user", user) \
+	.option("password", password) \
+	.option("driver", jdbc_driver) \
+	.load()
+	
+game_df.select("game_id", "local_date").show()
+game_df.createOrReplaceTempView("game")
+game_df.persist(StorageLevel.MEMORY_AND_DISK)
+
+#Initiate batter_view and calculate Hit/atBat value
 
 bat_avg = spark.sql(
 	"""
-	select
-	    batter
-	    , game_id
-	    , atBat
-	    , Hit 
-	    , (Hit/nullif(atBat,0)) as game_bat_avg
-	    , case when Hit > 0 then 1 else 0 end as ahit
-	from batter_view
-	order by batter, game_id
+				SELECT
+				batter
+				, game_id
+				, atBat
+				, Hit
+				, (Hit/nullif(atBat,0)) AS game_bat_avg
+				, CASE WHEN Hit > 0 THEN 1 else 0 END AS AH
+				FROM batter_view
+				ORDER BY batter, game_id
 	"""
 	)
 bat_avg.show()
 
-test_df = spark.sql(
-	    """
-	    SELECT
-	            *
-	            , SPLIT(CONCAT(
-	                CASE WHEN batter IS NULL THEN ""
-	                ELSE batter END,
-	                " ",
-	                CASE WHEN game_id IS NULL THEN ""
-	                ELSE game_id END,
-	                " ",
-	                CASE WHEN atBat IS NULL THEN ""
-	                ELSE atBat END,
-	                " ",
-	                CASE WHEN Hit IS NULL THEN ""
-	                ELSE Hit END
-	            ), " ") AS categorical
-	        FROM batter_view
-	    """
+# Rolling dataframe to calculate rolling batting average
+
+rolling_df = spark.sql(
+		"""
+		        SELECT
+	            b.batter
+	            , b.game_id
+	            , g.local_date
+	            , SUM(b.Hit) AS Total_Hit
+	            , SUM(b.atBat) AS Total_atBat
+	            FROM game g JOIN
+	            batter_view b ON
+	            b.game_id = g.game_id AND
+	            g.local_date BETWEEN DATE_SUB(g.local_date, 100) AND
+	            g.local_date
+	            GROUP BY
+	            b.batter, b.game_id, g.local_date
+		"""
 	)
+rolling_df.show()
+
+
+#Transform() converts 1 dataframe to another , Here we implement custom Transformer.
+#Kwargs help to give keyword arguments,@keyword_only  will store input keyword arguments
+
+class BattingAverageTransform(
+    Transformer,
+    HasInputCols,
+    HasOutputCol,
+    DefaultParamsReadable,
+    DefaultParamsWritable,
+):
+	#Constructor: set values for all Param objects
+    @keyword_only
+    def __init__(self, inputCols=None, outputCol=None):
+        super(BattingAverageTransform, self).__init__()
+        kwargs = self._input_kwargs
+        self.setParams(**kwargs)
+        return
+
+    @keyword_only
+    def setParams(self, inputCols=None, outputCol=None):
+        kwargs = self._input_kwargs
+        return self._set(**kwargs)
+        
+	#The input passed to  _transform() is the entire input Dataset
+    def _transform(self, dataset):
+        input_cols = self.getInputCols()
+        output_col = self.getOutputCol()
+        dataset = dataset.withColumn(output_col, round(dataset[input_cols[0]] / dataset[input_cols[1]], 3))
+        return dataset
+
+batting_avg_transform = BattingAverageTransform(
+	    inputCols=["Total_Hit", "Total_atBat"], outputCol="Batting_Average"
+)
+rolling_average = batting_avg_transform.transform(rolling_df)
+rolling_average.show()
+
+#create column and put into a single column for modeling -Categorical by encoding
+
+test_df = spark.sql(
+	        """
+	        SELECT
+	                *
+	                , SPLIT(CONCAT(
+	                    CASE WHEN batter IS NULL THEN ""
+	                    ELSE batter END,
+	                    " ",
+	                    CASE WHEN game_id IS NULL THEN ""
+	                    ELSE game_id END,
+	                    " ",
+	                    CASE WHEN atBat IS NULL THEN ""
+	                    ELSE atBat END,
+	                    " ",
+	                    CASE WHEN Hit IS NULL THEN ""
+	                    ELSE Hit END
+	                ), " ") AS categorical
+	            FROM batter_view
+	        """
+	    )
 test_df.show()
+
+#Count Vectorizer- to tranform data into vectors /Also enables preprocessing of data
 
 count_vectorizer = CountVectorizer(
 	inputCol="categorical", outputCol="categorical_vector"
@@ -100,7 +177,9 @@ count_vectorizer = CountVectorizer(
 count_vectorizer_fitted = count_vectorizer.fit(test_df)
 test_df = count_vectorizer_fitted.transform(test_df)
 test_df.show()
-    
+
+#Random Forest Classifier used to merge different decisions and make accurate prediction
+
 random_forest = RandomForestClassifier(
 labelCol="Hit",
 featuresCol="categorical_vector",
@@ -111,6 +190,10 @@ rawPredictionCol="raw_pred_hit",
 )
 
 random_forest_fitted = random_forest.fit(test_df)
-test_df = random_forest_fitted.transform(test_df)
-test_df.select("game_id", "batter","Hit","atBat","categorical").show()
+rolling_df = random_forest_fitted.transform(test_df)
+
+#Final Output
+
+rolling_df.select("game_id", "batter","Hit","atBat","categorical","categorical_vector").show()
+
     
