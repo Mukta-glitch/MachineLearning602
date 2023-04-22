454b24b10ad55dee3646f984c55c37a26f2a54dd
Mukta-Glitch <112215589+Mukta-glitch@users.noreply.github.com>
2023-04-22 04:10:58 -0700

    Final Commit Homework 5

--- scripts/hw_05.sql
+++ scripts/hw_05.sql
@@ -5,22 +5,27 @@ use baseball;
 drop 
   table IF EXISTS home_team_wins;
 
--- The home_team_wins column will be 1 if the home team wins, 0 if the away team wins, and NULL if the game was a tie
-
 CREATE table home_team_wins AS 
 SELECT 
-  game_id, 
-  CASE WHEN away_runs < home_runs THEN 1 WHEN away_runs > home_runs THEN 0 ELSE 0 END AS home_team_wins 
+  pc.game_id, 
+  pc.team_id,
+  CASE WHEN b.away_runs < b.home_runs THEN 1 WHEN b.away_runs > b.home_runs THEN 0 ELSE 0 END AS home_team_wins 
 FROM 
-  boxscore;
--- Create an index on the game_id column of the home_team_wins table for faster querying
+  boxscore b 
+JOIN 
+  pitcher_counts pc 
+ON 
+  pc.game_id = b.game_id 
+GROUP BY 
+  pc.team_id;
+
 CREATE INDEX index_game_id ON home_team_wins(game_id);
 
 describe home_team_wins;
 
 drop 
   table IF EXISTS joined;
--- Drop the joined table if it exists, then create a new one with various columns from multiple tables
+
 CREATE table joined AS 
 SELECT 
   i.inning_id, 
@@ -71,7 +76,6 @@ SELECT
     ), 
     0
   ) AS Walk_plus_Hits_per_Inning_Pitched, 
-  -- Calculate various derived columns and give them aliases
   COALESCE(
     9 * (
       b.away_runs / NULLIF(
@@ -136,85 +140,64 @@ describe joined;
 
 CREATE INDEX index_team_id on joined(team_id);
 
-drop table if exists rolling;
+#drop table if exists rolling;
 
-CREATE TABLE rolling as 
+CREATE TEMPORARY TABLE team_avg AS
 SELECT 
   team_id, 
-  AVG(Strike_To_Walk_Ratio) OVER (
-    PARTITION BY team_id 
-    ORDER BY 
-      local_date ROWS BETWEEN 9 PRECEDING 
-      AND CURRENT ROW
-  ) AS rolling_Strike_To_Walk_Ratio, 
-  AVG(
-    Walk_plus_Hits_per_Inning_Pitched
-  ) OVER (
-    PARTITION BY team_id 
-    ORDER BY 
-      local_date ROWS BETWEEN 9 PRECEDING 
-      AND CURRENT ROW
-  ) AS rolling_Walk_plus_Hits_per_Inning_Pitched, 
-  AVG(Earned_Run_Average) OVER (
-    PARTITION BY team_id 
-    ORDER BY 
-      local_date ROWS BETWEEN 9 PRECEDING 
-      AND CURRENT ROW
-  ) AS rolling_Earned_Run_Average, 
-  AVG(Slugging_Percentage) OVER (
-    PARTITION BY team_id 
-    ORDER BY 
-      local_date ROWS BETWEEN 9 PRECEDING 
-      AND CURRENT ROW
-  ) AS rolling_Slugging_Percentage, 
-  AVG(Times_on_Base) OVER (
-    PARTITION BY team_id 
-    ORDER BY 
-      local_date ROWS BETWEEN 9 PRECEDING 
-      AND CURRENT ROW
-  ) AS rolling_Times_on_Base, 
-  AVG(Run_Differential) OVER (
-    PARTITION BY team_id 
-    ORDER BY 
-      local_date ROWS BETWEEN 9 PRECEDING 
-      AND CURRENT ROW
-  ) AS rolling_Run_Differential, 
-  AVG(innings_pitched) OVER (
-    PARTITION BY team_id 
-    ORDER BY 
-      local_date ROWS BETWEEN 9 PRECEDING 
-      AND CURRENT ROW
-  ) AS rolling_innings_pitched, 
-  AVG(On_Base_Percentage) OVER (
-    PARTITION BY team_id 
-    ORDER BY 
-      local_date ROWS BETWEEN 9 PRECEDING 
-      AND CURRENT ROW
-  ) AS rolling_On_Base_Percentage, 
-  AVG(Batting_Average) OVER (
-    PARTITION BY team_id 
-    ORDER BY 
-      local_date ROWS BETWEEN 9 PRECEDING 
-      AND CURRENT ROW
-  ) AS rolling_Batting_Average, 
-  AVG(home_wp) OVER (
-    PARTITION BY team_id 
-    ORDER BY 
-      local_date ROWS BETWEEN 9 PRECEDING 
-      AND CURRENT ROW
-  ) AS rolling_home_wp, 
-  AVG(away_wp) OVER (
-    PARTITION BY team_id 
-    ORDER BY 
-      local_date ROWS BETWEEN 9 PRECEDING 
-      AND CURRENT ROW
-  ) AS rolling_away_wp 
+  local_date,
+  AVG(Slugging_Percentage) AS avg_Slugging_Percentage,
+  AVG(away_wp) AS Avg_away_wp,
+  AVG(home_wp) AS Avg_home_wp,
+  AVG(HomeRuns_per_9_Innings) As Avg_HomeRuns_per_9_innings,
+  AVG(Batting_Average) AS Avg_batting_average,
+  AVG(On_Base_Percentage) As Avg_On_Base_Percentage,
+  AVG(Run_Differential) As Avg_Run_Differential,
+  AVG(Times_on_Base) As Avg_Times_on_Base,
+  AVG(Earned_Run_Average) As Avg_Earned_Run_Average,
+  AVG(innings_pitched) As Avg_innings_pitched,
+  Avg(Walk_plus_Hits_per_Inning_Pitched) As Avg_Walk_plus_Hits_per_Inning_Pitched,
+  Avg(Strike_To_Walk_Ratio) As Avg_Strike_To_Walk_Ratio
 FROM 
-  joined 
-WHERE 
-  local_date BETWEEN DATE_SUB(local_date, INTERVAL 100 DAY) 
-  AND local_date 
-group by 
-  team_id;
+  joined
+GROUP BY 
+  team_id, 
+  local_date;
 
-  select * from rolling;
+drop 
+  table IF EXISTS rolling;
+
+CREATE TABLE rolling AS 
+SELECT 
+  t1.team_id,
+  t1.local_date,
+  AVG(t2.avg_Slugging_Percentage) AS rolling_Slugging_Percentage,
+  AVG(t2.Avg_innings_pitched) AS rolling_innings_pitched,
+  AVG(t2.Avg_away_wp) As rolling_away_wp,
+  AVG(t2.Avg_home_wp) As rolling_home_wp,
+  AVG(t2.Avg_Strike_To_Walk_Ratio) As rolling_Strike_To_Walk_Ratio,
+  AVG(t2.Avg_Walk_plus_Hits_per_Inning_Pitched) As rolling_Walk_plus_Hits_per_Inning_Pitched,
+  AVG(t2.Avg_HomeRuns_per_9_Innings) As rolling_HomeRuns_per_9_Innings,
+  AVG(t2.Avg_Batting_Average) As rolling_Batting_Average,
+  AVG(t2.Avg_Times_on_Base) As rolling_Times_On_Base,
+  AVG(t2.Avg_Earned_Run_Average) As rolling_Earned_Run_Average,
+  AVG(t2.Avg_On_Base_Percentage) As rolling_On_Base_Percentage,
+  AVG(t2.Avg_Run_Differential) As rolling_run_differential
+FROM 
+  team_avg AS t1 
+  JOIN team_avg AS t2 ON (
+    t1.team_id = t2.team_id AND 
+    t2.local_date BETWEEN DATE_SUB(t1.local_date, INTERVAL 100 DAY) AND t1.local_date
+  )
+GROUP BY 
+  t1.team_id;
+
+drop table if exists joined1;
+create table joined1 as select 
+  r.team_id,r.rolling_away_wp,r.rolling_run_differential,r.rolling_home_wp,r.rolling_Batting_Average,r.rolling_innings_pitched,
+  r.rolling_Slugging_Percentage,r.rolling_Times_On_Base,r.rolling_Earned_Run_Average,r.rolling_On_Base_Percentage,r.rolling_HomeRuns_per_9_Innings,
+  r.rolling_Walk_plus_Hits_per_Inning_Pitched,r.rolling_Strike_To_Walk_Ratio,h.home_team_wins
+ from rolling r join home_team_wins h on r.team_id=h.team_id;
+select * from joined1;
+#select * from rolling;
+
